f2c5666f153848ecf5049ec7908584547b3fc28e
Razakhan15 <92670281+Razakhan15@users.noreply.github.com>
2024-03-31 10:50:11 +0530

    cha

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,10 @@ app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
-const __dirname1 = path.resolve("/coding/web development/prac/live chat using db");
+const __dirname1 = path.resolve();
 console.log(__dirname1);
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "frontend", "build")));
+  app.use(express.static(path.join(__dirname1,'..', "frontend", "build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
   });
